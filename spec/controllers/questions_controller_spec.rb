0715b3479376f1c09d00817f18ac21dc74c87b0a
theprosto <sitennov@mail.ru>
2017-05-17 20:21:44 +0300

    refactor: QuestionsControllerSpec

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
@@ -16,8 +18,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
-
     before { get :show, params: { id: question.id } }
 
     it 'assings the requested question to @question' do
@@ -42,8 +42,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
-    let(:question) { create(:question) }
-
     before { get :edit, params: { id: question.id } }
 
     it 'assings the requested question to @question' do
