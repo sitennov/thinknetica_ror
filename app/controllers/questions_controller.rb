fcd818a3eb1ecdd1977ab540e426de4f0096a60f
theprosto <sitennov@mail.ru>
2017-05-17 22:53:05 +0300

    feature: action destroy

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :get_question, only: [:show, :edit, :update]
+  before_action :get_question, only: [:show, :edit, :update, :destroy]
 
   def index
     @questions = Question.all
@@ -30,6 +30,11 @@ class QuestionsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
