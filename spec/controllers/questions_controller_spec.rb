0fd7835ceab05a5082522a3245bbb920cf460d3f
theprosto <sitennov@mail.ru>
2017-05-17 19:41:48 +0300

    feature: action show

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -4,9 +4,7 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
 
     it 'populates an array of all quetions' do
       expect(assigns(:questions)).to match_array(@questions)
@@ -14,6 +12,20 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'render index view' do
       expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    let(:question) { create(:question) }
+
+    before { get :show, params: { id: question.id } }
+
+    it 'assings the requested question to @question' do
+      expect(assigns(:question)).to eq(question)
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
     end
   end
 end
