1bd28f3a4f1c07e0c7af8eb7bed8fd04d497134f
theprosto <sitennov@mail.ru>
2017-05-17 22:34:57 +0300

    feature: action update

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :get_question, only: [:show, :edit]
+  before_action :get_question, only: [:show, :edit, :update]
 
   def index
     @questions = Question.all
@@ -21,6 +21,14 @@ class QuestionsController < ApplicationController
       redirect_to question_path(@question)
     else
       render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to question_path(@question)
+    else
+      render :edit
     end
   end
 
