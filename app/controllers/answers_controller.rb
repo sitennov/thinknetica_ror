6f98dfec3b36824fd5e70aed50afa84f250e8a45
theprosto <sitennov@mail.ru>
2017-05-18 14:30:44 +0300

    fix: answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,8 @@
 class AnswersController < ApplicationController
   def create
     @question = Question.find(params[:question_id])
-    @question.answers.create(answer_params)
+    @answer = @question.answers.new(answer_params)
+    @answer.save
     redirect_to question_path(@question)
   end
 
