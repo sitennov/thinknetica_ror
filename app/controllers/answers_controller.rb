70141961567c96c8b91ec81b98eaf3890c51603a
theprosto <sitennov@mail.ru>
2017-05-26 18:44:00 +0300

    feature: add user to answers, questions...

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,21 +1,28 @@
 class AnswersController < ApplicationController
-  def create
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.new(answer_params)
-    @answer.save
+  before_action :get_question
+
+  def new
+    @answer = @question.answers.new
   end
 
   def create
-    @question = Question.find(params[:question_id])
-    @answer = @question.answers.new(answer_params)
+    @answer = @question.answers.create(answer_params)
     if @answer.save
-      redirect_to question_path(@question)
+      redirect_to question_answers_path(@question)
     else
-      render 'questions/show'
+      render :new
     end
   end
 
+  def index
+    @answers = @question.answers
+  end
+
   private
+
+  def get_question
+    @question = Question.find(params[:question_id])
+  end
 
   def answer_params
     params.require(:answer).permit(:body)
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -32,6 +32,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #new' do
+    sign_in_user
+
     before { get :new }
 
     it 'assigns a new question to @question' do
@@ -44,6 +46,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #edit' do
+    sign_in_user
+
     before { get :edit, params: { id: question.id }}
 
     it 'assings the requested question to @question' do
@@ -56,6 +60,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'POST #create' do
+    sign_in_user
+
     context 'with valid attributes' do
       it 'saves the new question' do
         expect { post :create, params: { question: attributes_for(:question) }
@@ -83,6 +89,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'PATCH #update' do
+    sign_in_user
+
     context 'valid attributes' do
       it 'assings the requested question to @question' do
         patch :update, params: { id: question.id,
@@ -125,6 +133,8 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'DELETE #destroy' do
+    sign_in_user
+
     it 'deletes question' do
       question
       expect {
