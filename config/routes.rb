cb2d4a07a3cd17b6e8c2c23a2e5872bd97304a16
theprosto <sitennov@mail.ru>
2017-06-02 01:59:25 +0300

    use shallow to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'questions#index'
   devise_for :users
 
-  resources :questions do
-    resources :answers
+  resources :questions, shallow: true do
+    resources :answers, only: [:create, :destroy]
   end
 end
