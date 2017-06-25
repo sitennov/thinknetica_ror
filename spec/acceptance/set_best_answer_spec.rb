bd5146701cef9a3bfa8ef8356da51a15d2ad57ad
theprosto <sitennov@mail.ru>
2017-06-25 18:58:09 +0300

    feature: add set_best_spec

--- spec/acceptance/set_best_answer_spec.rb
+++ spec/acceptance/set_best_answer_spec.rb
@@ -6,7 +6,7 @@ feature 'Set best answer', %q{
   I want to be able from choose the best answer to my question
 } do
 
-  given(:user) { create(:user) }
+  given!(:user) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:answers) { create_list(:answer, 3, question: question, user: user) }
 
@@ -18,37 +18,28 @@ feature 'Set best answer', %q{
     expect(page).to_not have_button 'Set best'
   end
 
-  describe 'Authenticated user' do
+  describe 'Authenticated user', js: true do
     before do
       sign_in(user)
       visit question_path(question)
     end
-    scenario 'can set best asnwer', js: true do
+
+    scenario 'trying set best answer' do
       within "#answer-#{answers.first.id}" do
         click_on 'Set best'
-        expect(page).to have_content 'BEST'
       end
-    end
-    scenario 'can set another answer as best', js: true do
-      within "#answer-#{answers.first.id}" do
-        click_on 'Set best'
-        expect(page).to have_content 'BEST'
-      end
-      within "#answer-#{answers.second.id}" do
-        click_on 'Set best'
-        expect(page).to have_content 'BEST'
-      end
-    end
-    scenario 'see best answer first in answers list', js: true do
-      within "#answer-#{answers.third.id}" do
-        click_on 'Set best'
-      end
-      expect(page).to have_content 'Answers'
-      best_tr = page.find('.answr').first(:xpath, 'tr[1]')
-      within best_tr do
-        expect(page).to have_content 'BEST'
-      end
+      expect(page).to have_selector('.answer-best')
+      expect(page).to have_selector('.answer-best', count: 1)
     end
 
+    scenario 'trying set best answer of another user' do
+      within "#answer-#{answers.last.id}" do
+        click_on 'Set best'
+      end
+
+      within ".answers" do
+        expect(find('.answer:first-child')).to have_selector('.answer-best')
+      end
+    end
   end
 end
