a873524277b80c22d96e85660a4428eb5757654f
theprosto <sitennov@mail.ru>
2017-07-03 20:06:14 +0300

    feature: Attachment of files to question

--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -15,12 +15,13 @@ feature 'Add files to question', %q{
 
   scenario 'User adds file when asks question' do
     fill_in 'Title', with: 'Test question'
-    fill_in 'Text', with: 'text text text'
+    fill_in 'Body', with: 'text text text'
 
     attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
 
     click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'spec_helper.rb',
+      href: '/uploads/attachment/file/1/spec_helper.rb'
   end
 end
