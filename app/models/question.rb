4e526b6fcd5eb4522052d8dca63672a315da3e9c
theprosto <sitennov@mail.ru>
2017-07-03 20:06:14 +0300

    feature: Attachment of files to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,6 @@ class Question < ApplicationRecord
   belongs_to :user
   has_many :answers, dependent: :destroy
   has_many :attachments, as: :attachable, dependent: :destroy
+
+  accepts_nested_attributes_for :attachments
 end
