03c8c0628c99a1cf258bf687433f2102dd730f3e
theprosto <sitennov@mail.ru>
2017-07-04 16:25:46 +0300

    feature: add many attachments to answers

--- spec/acceptance/add_files_to_answer_spec.rb
+++ spec/acceptance/add_files_to_answer_spec.rb
@@ -17,10 +17,14 @@ feature 'Add files to answer', %q{
   scenario 'User adds a file to the answer', js: true do
     fill_in 'Your answer', with: 'My answer'
 
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add attachment'
+
+    all('input[type="file"]').first.set("#{Rails.root}/spec/rails_helper.rb")
+    all('input[type="file"]').last.set("#{Rails.root}/spec/spec_helper.rb")
+
     click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb',
-      href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb'
+    expect(page).to have_link 'spec_helper.rb'
   end
 end
