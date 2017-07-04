e8958cd8b333ae074f489ede2c9c1a357e5a2d5f
theprosto <sitennov@mail.ru>
2017-07-04 16:19:51 +0300

    feature: use gem cocoon for many attachments to questions

--- spec/acceptance/add_files_to_question_spec.rb
+++ spec/acceptance/add_files_to_question_spec.rb
@@ -13,15 +13,18 @@ feature 'Add files to question', %q{
     visit new_question_path
   end
 
-  scenario 'User adds file when asks question' do
+  scenario 'User adds file when asks question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
 
-    attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+    click_on 'add attachment'
+
+    all('input[type="file"]').first.set("#{Rails.root}/spec/rails_helper.rb")
+    all('input[type="file"]').last.set("#{Rails.root}/spec/spec_helper.rb")
 
     click_on 'Create'
 
-    expect(page).to have_link 'spec_helper.rb',
-      href: '/uploads/attachment/file/1/spec_helper.rb'
+    expect(page).to have_link 'rails_helper.rb'
+    expect(page).to have_link 'spec_helper.rb'
   end
 end
