a18d44d5cd3b3437c19d8c1f6ca1cd7674ca3e75
theprosto <sitennov@mail.ru>
2017-07-04 21:09:32 +0300

    feature: user deletes attachments to question, fix: acceptance view question and answers spec

--- spec/acceptance/view_question_and_answers_spec.rb
+++ spec/acceptance/view_question_and_answers_spec.rb
@@ -6,18 +6,32 @@ feature 'view question and answers', %q{
   I want to be able to view the questions and answers to it
 } do
 
+  given(:user) { create(:user) }
   given(:question) { create(:question) }
   given(:answers) { create_list(:answer, 2, question: question) }
 
-  scenario 'View question and reply to it' do
+  scenario 'Authenticated user view question and answers' do
+    sign_in(user)
     answers
     visit question_path(question)
 
     expect(page).to have_content question.title
     expect(page).to have_content question.body
 
-    page.all('.panel-body').each_with_index do |el, i|
-      expect(el).to have_content(answers[i].body)
+    answers.each do |answer|
+      expect(page).to have_content answer.body
+    end
+  end
+
+  scenario 'Non-authenticated user view question and answers' do
+    answers
+    visit question_path(question)
+
+    expect(page).to have_content question.title
+    expect(page).to have_content question.body
+
+    answers.each do |answer|
+      expect(page).to have_content answer.body
     end
   end
 end
