2ef0f9bac460bb9e8af8a957cace4b976b17a29c
theprosto <sitennov@mail.ru>
2017-07-11 19:51:26 +0300

    add: concern votable and spec

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :votable, polymorphic: true, optional: true
+
+  validates :votable_id, :votable_type, :value, presence: true
+  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type] }
 end
