6b2cdabd35b2b8cf9b40121789e7cce1491e2df9
theprosto <sitennov@mail.ru>
2017-08-04 13:53:24 +0300

    use capybara_email

--- spec/acceptance/authenticate_oauth_spec.rb
+++ spec/acceptance/authenticate_oauth_spec.rb
@@ -10,19 +10,20 @@ feature 'Authenticate using oauth', %q{
 
   describe 'Authenticate user using Facebook', js: true do
     scenario 'New user first time try to login using facebook' do
-      mock_auth_hash(:facebook)
+      clear_emails
+      auth = mock_auth_hash(:facebook)
+
       visit new_user_session_path
       click_on 'Sign in with Facebook'
 
       expect(page).to have_content('You have to confirm your email address before continuing.')
 
-      message = ActionMailer::Base.deliveries.last.body.raw_source
-      doc = Nokogiri::HTML.parse(message)
-      url = doc.css("a").map { |link| link[:href] }.first
-      visit url
+      open_email(auth[:info][:email])
 
+      current_email.click_link 'Confirm my account'
       expect(page).to have_content('Your email address has been successfully confirmed')
 
+      visit new_user_session_path
       click_on 'Sign in with Facebook'
 
       expect(page).to have_content('Successfully authenticated from Facebook account.')
@@ -41,23 +42,23 @@ feature 'Authenticate using oauth', %q{
 
   describe 'Authenticate user using Twitter', js: true do
     scenario 'New user first time try to login using twitter' do
-      mock_auth_hash(:twitter, nil)
+      clear_emails
       visit new_user_session_path
-
+      mock_auth_hash(:twitter, nil)
       click_on 'Sign in with Twitter'
+
       expect(page).to have_content 'Email is required to compete sign up'
       fill_in 'Email', with: 'twitter@mail.ru'
       click_on 'Submit'
 
       expect(page).to have_content('You have to confirm your email address before continuing.')
 
-      message = ActionMailer::Base.deliveries.last.body.raw_source
-      doc = Nokogiri::HTML.parse(message)
-      url = doc.css("a").map { |link| link[:href] }.first
-      visit url
+      open_email('twitter@mail.ru')
 
-      expect(page).to have_content('Your email address has been successfully confirmed')
+      current_email.click_link 'Confirm my account'
+      expect(page).to have_content('Your email address has been successfully confirmed.')
 
+      visit new_user_session_path
       click_on 'Sign in with Twitter'
 
       expect(page).to have_content('Successfully authenticated from Twitter account')
