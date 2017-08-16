c979a51e6aa0f4ccd3a74847f16ceb5ac06e5df2
theprosto <sitennov@mail.ru>
2017-08-16 12:36:58 +0300

    add api v1 profiles index

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -4,12 +4,20 @@ class Api::V1::ProfilesController < ApplicationController
   respond_to :json
 
   def me
-    respond_with current_resource_owner
+    respond_with(current_resource_owner)
+  end
+
+  def index
+    respond_with(User.where.not(id: current_resource_owner.id))
   end
 
   protected
 
   def current_resource_owner
     @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
+  def current_user
+    current_resource_owner
   end
 end
