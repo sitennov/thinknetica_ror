081766fbd2a5d89a352b7d6b14f902cdb8b396be
theprosto <sitennov@mail.ru>
2017-08-18 10:59:02 +0300

    add API for questions

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,7 +1,5 @@
-class Api::V1::ProfilesController < ApplicationController
-  before_action :doorkeeper_authorize!
-  skip_authorization_check
-  respond_to :json
+class Api::V1::ProfilesController < Api::V1::BaseController
+  authorize_resource :user
 
   def me
     respond_with(current_resource_owner)
@@ -9,15 +7,5 @@ class Api::V1::ProfilesController < ApplicationController
 
   def index
     respond_with(User.where.not(id: current_resource_owner.id))
-  end
-
-  protected
-
-  def current_resource_owner
-    @current_resource_owner ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
-  end
-
-  def current_user
-    current_resource_owner
   end
 end
