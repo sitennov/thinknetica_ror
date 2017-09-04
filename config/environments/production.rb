f5b82a3b810c0a3dc744faab8f2aa977c5e57cef
mike <sitennov@mail.ru>
2017-09-04 16:12:28 +0300

    actionCable conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   #     :password             => ''
   # }
 
-  config.action_mailer.default_url_options = { host: 'qna.net.ru'}
+  config.action_mailer.default_url_options = { host: '188.226.177.95'}
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
@@ -57,9 +57,8 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
-  config.action_cable.allowed_request_origins = ["http://188.226.177.95"]
+  # config.action_cable.allowed_request_origins = ['http://188.226.177.95']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
